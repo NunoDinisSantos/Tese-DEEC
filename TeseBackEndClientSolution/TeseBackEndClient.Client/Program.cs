7ac1ce8a642cec0394e223f7bb48b20757611fdf
NunoDinisSantos <nunomiguelps1@sapo.pt>
2024-12-10 22:52:41 +0000

    Commits TeseBackEndClientSolution

---
+++ TeseBackEndClientSolution/TeseBackEndClient.Client/Program.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44335/") }); // TO CHANGE
+
+await builder.Build().RunAsync();
+
---
+++ TeseBackEndClientSolution/TeseBackEndClient/Services/RegisterService.cs
@@ -0,0 +1,18 @@
+﻿namespace TeseBackEndClient.RegisterService
+{
+    public class RegisterService
+    {
+        private readonly HttpClient _httpClient;
+
+        public RegisterService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> RegisterStudentAsync(string numeroEstudante)
+        {
+            var response = await _httpClient.PostAsJsonAsync("https://localhost:44335/api/misteriosaquaticos/2013168188", numeroEstudante);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}

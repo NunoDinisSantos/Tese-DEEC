b11511e0451e1a9defa2c77d6c0833157d6a047d
NunoDinisSantos <nunomiguelps1@sapo.pt>
2024-12-26 21:22:28 +0000

    #39 - Trying other IP to see if outside connection works.

--- Tese_Blazor/TeseBlazor.Client/Program.cs
+++ Tese_Blazor/TeseBlazor.Client/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://172.19.0.3:8081/") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://172.19.0.3:8081/") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44335/") }); // TO CHANGE
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://88.198.115.159/") }); // TO CHANGE
 
 await builder.Build().RunAsync();
--- Tese_Blazor/TeseBlazor/Program.cs
+++ Tese_Blazor/TeseBlazor/Program.cs
@@ -6,7 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://172.19.0.3:8081/") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://172.19.0.3:8081/") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44335/") }); // TO CHANGE
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://88.198.115.159/") }); // TO CHANGE
 
 builder.WebHost.ConfigureKestrel(options =>
 {

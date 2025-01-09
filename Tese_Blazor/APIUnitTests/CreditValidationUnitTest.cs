f93acb27cfa4d77bb0efed7b7afa4fb346a1f0b3
NunoDinisSantos <nunomiguelps1@sapo.pt>
2025-01-09 13:32:57 +0000

    #62 - Adds Unit tests for CreditValidation (xUnit)

---
+++ Tese_Blazor/APIUnitTests/CreditValidationUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using TeseAPIs.Validations;
+
+namespace APIUnitTests
+{
+    public class CreditValidationUnitTest
+    {
+        [Theory]
+        [InlineData(100,50)]
+        [InlineData(100,-50)]
+        [InlineData(100, -100)]
+        public void VerifyNoNegativeAmounts_ShouldUpdateReturnsTrue_WhenAddingCredits(
+            int studentSavedCredits, int creditsToSum)
+        {
+            var updater = new CreditValidations();
+
+            var result = updater.VerifyNoNegativeAmounts(studentSavedCredits, creditsToSum);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void VerifyNoNegativeAmounts_ShouldUpdateReturnsFalse_WhenSubtractingCreditsAreMoreThanTotalCredits()
+        {
+            var updater = new CreditValidations();
+            int studentSavedCredits = 100;
+            int creditsToSum = -101;
+
+            var result = updater.VerifyNoNegativeAmounts(studentSavedCredits, creditsToSum);
+
+            Assert.False(result);
+        }
+    }
+}
